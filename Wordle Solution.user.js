8e9198210d2c67e222c33a7e5434c8f0387e3b52
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-07-01 09:34:05 -0400

    Added Wordle Solution UserScript
    Installing this user script will allow you to hover over the Wordle logo at the top of the page and reveal the daily Wordle word to you. No longer will you have to sweat it out on the sixth move, praying to the gods of the lexicon to be on your side.

---
+++ Wordle Solution.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Wordle Solution
+// @namespace    http://github.com/Alex-M-Howard/UserSCripts
+// @version      1.0
+// @match        https://*.nytimes.com/games/wordle/*
+// @description  Don't let wordle intimidate you, for you will always win. Hover your mouse on the wordle logo at top of page and you will see the daily word!
+// @author       Alex Howard
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    const header = document.getElementsByTagName("header")
+
+    let timer = setInterval(function () {
+        if (header.length > 0){
+            console.log('Now we are here')
+            runMouseOver(timer)
+            //header[0].firstElementChild.nextElementSibling.innerText = solution
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+)()
+
+function runMouseOver(timer) {
+    clearInterval(timer);
+    const solution = (JSON.parse(localStorage['nyt-wordle-state']).solution).toUpperCase();
+    const header = document.getElementsByTagName("header")[0];
+    const logo = header.firstElementChild.nextElementSibling.innerText
+
+    header.addEventListener("mouseover", function (event) {
+        header.firstElementChild.nextElementSibling.innerText = solution;
+        return
+    })
+
+    header.addEventListener("mouseout", function (event) {
+        header.firstElementChild.nextElementSibling.innerText = logo;
+    })
+
+}
